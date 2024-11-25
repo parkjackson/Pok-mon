ea81f7eafb7a8a3dc4fce546936e57315cd69921
parkjackson <156553956+parkjackson@users.noreply.github.com>
2024-11-25 10:55:03 -0800

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -8,8 +8,13 @@ public class Pokemon {
     private String name; //What's the pokemons name? 
     private String type; //What's the pokemons type? (fire bug ground etc)
     private String secondaryType; //Does the pokemon have a secondaryType? 
-    private int evolutions; //How many possible evolutions does this Pokemon have?  (1 2 3 etc)
-    private String evolution stage; //1st-Stage 2nd-Stage 3rd-Stage (etc) OR Single-Stage. 
-    private String condition; //How does the pokemon evolve? (by level, by stone, by friendship, by trade). 
+    private boolean firstEvolution; //does this pokemon have a first evolution?
+    private boolean secondEvolution; //does this pokemon have a second evolution?
+    private boolean thirdEvolution; //does this pokemon have a third evolution?
+    private int height; //what is the height of the pokemon
   
 }
+
+
+
+ArrayList<Integer> myList = new ArrayList<>();
